31c78760487e0121015ae56d84b3f1ed3687851e
tomoe <cph-tp95@cphbusiness.dk>
2016-11-21 23:22:29 +0100

    Information to aggregator added

--- src/main/java/com/what/bankws/BankWS.java
+++ src/main/java/com/what/bankws/BankWS.java
@@ -1,11 +1,16 @@
 package com.what.bankws;
 
-import com.rabbitmq.client.AMQP.Channel;
+import com.rabbitmq.client.AMQP.*;
+import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -13,42 +18,82 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
 import javax.xml.namespace.QName;
 
 @WebService(serviceName = "InterestRateService")
 public class BankWS {
 
-    private final String EXCHANGENAME = "whatNormalizer";//change name later
-
     @WebMethod(operationName = "getInterestRate")
     public String getInterestRate(@WebParam(name = "ssn") String ssn,
-            @WebParam(name = "creditScore") int creditScore, @WebParam(name = "loanAmount") double loanAmount, @WebParam(name = "loanDuration") int loanDuration, String replyTo) throws JAXBException, IOException {
+            @WebParam(name = "creditScore") int creditScore,
+            @WebParam(name = "loanAmount") double loanAmount,
+            @WebParam(name = "loanDuration") int loanDuration,
+            @WebParam(name = "replyTo") String replyTo,
+            @WebParam(name = "corrId") String corrId,
+            @WebParam(name = "bankName") String bankName,
+            @WebParam(name = "total") int total,
+            @WebParam(name = "messageNo") int messageNo) {
 
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setUsername("what");
-        factory.setPassword("what");
-        Connection connection = factory.newConnection();
-        com.rabbitmq.client.Channel channel = connection.createChannel();
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("datdb.cphbusiness.dk");
+            factory.setUsername("what");
+            factory.setPassword("what");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            double reply = 0;
+            if (creditScore <= 100) {
+                reply = 12.02301;
+            }
+            else if (creditScore <= 150 && creditScore > 100) {
+                reply = 10.20155;
+            }
+            else if (creditScore <= 200 && creditScore > 150) {
+                reply = 9.2335;
+            }
+            else {
+                reply = 6.32265;
+            }
 
-        Random ran = new Random();
-        int intReply = ran.nextInt(1000 - 100 + 1) + 100;
-        double reply = intReply / 100d;
-        BankResponse res = new BankResponse(reply, ssn);
-        JAXBContext jc = JAXBContext.newInstance(BankResponse.class);
-        Marshaller marshaller = jc.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        JAXBElement<BankResponse> je2 = new JAXBElement(new QName("LoanRequest"), BankResponse.class, res);
-        StringWriter sw = new StringWriter();
-        marshaller.marshal(je2, sw);
-        String xmlString = sw.toString();
+            Map<String, Object> headers = new HashMap();
+            headers.put("bankName", bankName);
+            headers.put("total", total);
+            headers.put("messageNo", messageNo);
+            BasicProperties prop=propBuilder(corrId,headers);
+            BankResponse res = new BankResponse(reply, ssn);
+            JAXBContext jc = JAXBContext.newInstance(BankResponse.class);
+            Marshaller marshaller = jc.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            JAXBElement<BankResponse> je2 = new JAXBElement(new QName("LoanResponse"), BankResponse.class, res);
+            StringWriter sw = new StringWriter();
+            marshaller.marshal(je2, sw);
+            String xmlString = sw.toString();
+            channel.queueDeclare(replyTo, false, false, false, null);
+            channel.basicPublish("", replyTo, prop, xmlString.getBytes());
+            System.out.println("xml" + xmlString);
+            return xmlString;
+        }
+        catch (PropertyException ex) {
+            Logger.getLogger(BankWS.class.getName()).log(Level.SEVERE, null, ex);
+            return (ex.getMessage());
+        }
+        catch (JAXBException ex) {
+            Logger.getLogger(BankWS.class.getName()).log(Level.SEVERE, null, ex);
+            return ex.getMessage();
+        }
+        catch (IOException ex) {
+            Logger.getLogger(BankWS.class.getName()).log(Level.SEVERE, null, ex);
+            return ex.getMessage();
+        }
+    }
 
-        channel.basicPublish(EXCHANGENAME, "", null, xmlString.getBytes());
-
-        channel.close();
-        connection.close();
-
-        System.out.println("xml" + xmlString);
-        return xmlString;
+    private BasicProperties propBuilder(String corrId, Map<String, Object> headers) {
+        BasicProperties.Builder builder = new BasicProperties.Builder();
+        builder.correlationId(corrId);
+        builder.headers(headers);
+        BasicProperties prop = builder.build();
+        return prop;
     }
 
 }
